e211f86f2912a97277349531996645a54e291021
bibim11 <138500923+bibim11@users.noreply.github.com>
2026-09-05 00:55:16 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+    intents:[
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
+    ]
+});
+
+
+client.once("ready",()=>{
+    console.log(`Bot Online : ${client.user.tag}`);
+});
+
+
+client.login(process.env.TOKEN);
